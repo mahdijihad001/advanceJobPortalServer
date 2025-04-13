bc37b81c5e2a9d4377a43a6306ecc9c01a053a12
mahdiJihad <mahdijihad001@gmail.com>
2025-04-13 23:12:20 +0600

    Inplement password hashing lojgic

--- src/User/UserModel.js
+++ src/User/UserModel.js
@@ -1,28 +1,36 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    username : {
-        type : String,
-        required : [true , "Username is required"]
+    username: {
+        type: String,
+        required: [true, "Username is required"]
     },
-    email : {
-        type : String,
-        required : [true , "email is required"]
+    email: {
+        type: String,
+        required: [true, "email is required"]
     },
-    password : {
-        type : String,
-        required : [true , "password is required"]
+    password: {
+        type: String,
+        required: [true, "password is required"]
     },
-    role : {
-        type : String,
-        default : "candidate",
-        enum : ["candidate" , "employe" , "admin"]
+    role: {
+        type: String,
+        enum: ["candidate", "employe", "admin"],
+        default: "candidate"
     }
-} , {
-    timestamps : true
+}, {
+    timestamps: true
+});
+
+userSchema.pre("save" , async function(next){
+    if(!this.isModified("password")){
+        next();
+    };
+    this.password = await bcrypt.hash(this.password , 10);
 });
 
 
-const userModel = mongoose.model("user" , userSchema);
+const userModel = mongoose.model("user", userSchema);
 
-module.exports = {userModel};
+module.exports = { userModel };
