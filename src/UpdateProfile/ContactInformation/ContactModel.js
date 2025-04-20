1c517e252879f606fedbbd934d0850b47427522e
mahdiJihad <mahdijihad001@gmail.com>
2025-04-20 11:32:14 +0600

    Make profile all model & validation

--- src/UpdateProfile/ContactInformation/ContactModel.js
+++ src/UpdateProfile/ContactInformation/ContactModel.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
+    
+    country : String,
+    city : String,
+    compliteAddress : String
 
 } , {timestamps : true , versionKey : false});
 
--- src/UpdateProfile/SocialNetwork/NetworkModel.js
+++ src/UpdateProfile/SocialNetwork/NetworkModel.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const networkSchema = new mongoose.Schema({
-
+    facebook : String,
+    twiter : String,
+    linkedin : String,
+    github : String
 } , {timestamps : true , versionKey : false});
 
 const networkModel = mongoose.model("network" , networkSchema);
