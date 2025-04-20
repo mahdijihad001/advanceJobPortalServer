5c715d0877fe46cb910037867a242f74615ac0ee
mahdiJihad <mahdijihad001@gmail.com>
2025-04-20 23:30:48 +0600

    Build profile update controller

---
+++ src/UpdateProfile/ContactInformation/ContactController.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const contactModel = require("./ContactModel");
+
+
+const updateSocialNetwork = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).send({ status: false, message: "User not valid! Please try again." });
+        };
+
+        const result = await contactModel.findByIdAndUpdate(id, { ...req.body }, { new: true, upsert: true, runValidators: true });
+
+        if (!result) {
+            return res.status(400).send({ status: false, message: "Profile update faild!" });
+        };
+
+        res.status(200).send({ status: true, message: "Profile updated success" });
+
+    } catch (error) {
+        return res.status(401).send({ status: false, message: "Profile update faild!" });
+    }
+};
+
+module.exports = {updateSocialNetwork};
