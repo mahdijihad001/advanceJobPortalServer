a06542110926a7f4b7c6efab2dad40f23d291b7f
mahdiJihad <mahdijihad001@gmail.com>
2025-04-26 22:27:03 +0600

    Build company model & apis

---
+++ src/Company/companyController.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const companyModel = require("./companyModel");
+
+const updateCompanyController = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).send({ status: false, message: "User not valid!" });
+        };
+        
+        await companyModel.findByIdAndUpdate(id, { ...req.body }, { new: true, runValidators: true });
+        res.status(200).send({ status: true, message: "Company Update Success" });
+
+    } catch (error) {
+        return res.status(500).send({ status: false, message: "Conpany Update faild!" });
+    }
+}
