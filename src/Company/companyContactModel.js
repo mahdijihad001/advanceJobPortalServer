22ae773cbb13d0782bd61e42857e5a623438557b
mahdiJihad <mahdijihad001@gmail.com>
2025-04-27 09:51:49 +0600

    Build Company apis

--- src/Company/companyContactModel.js
+++ src/Company/companyContactModel.js
@@ -5,16 +5,18 @@ const companySchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         required: [true, "Authore are required!"]
     },
-    country : {
-        type : String ,
-        required : [true , "Country name are required!"]
+    country: {
+        type: String,
+        required: [true, "Country name are required!"]
     },
-    city : {
-        type : String,
-        required : [true , "City name are required!"]
+    city: {
+        type: String,
+        required: [true, "City name are required!"]
     },
-    compliteAddress : String,
-    required : [true , "Company complete address are required!!"]
+    compliteAddress: {
+        type: String,
+        required: [true, "Company complete address are required!!"]
+    }
 }, {
     timestamps: true,
     versionKey: false
--- src/Company/companyController.js
+++ src/Company/companyController.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const companyModel = require("./companyModel");
+const companyNetworkModel = require("./companyNetworkModel");
 
 // Company Controller
 
@@ -11,10 +12,36 @@ const updateCompanyController = async (req, res) => {
             return res.status(400).send({ status: false, message: "User not valid!" });
         };
 
-        await companyModel.findByIdAndUpdate(id, { ...req.body }, { new: true, runValidators: true });
+        const result = await companyModel.findByIdAndUpdate(id, { ...req.body }, { new: true, runValidators: true });
+        if (!result) {
+            return res.status(501).send({ status: false, message: "Conpany not update!" });
+        }
         res.status(200).send({ status: true, message: "Company Update Success" });
 
     } catch (error) {
         return res.status(500).send({ status: false, message: "Conpany Update faild!" });
     }
-}
+};
+
+// Company Social Network
+
+const companySocialNetworkUpdate = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).send({ status: false, message: "User not valid!" });
+        };
+
+        const result = await companyNetworkModel.findByIdAndUpdate(id, { ...req.body }, { new: true, runValidators: true });
+
+        if (!result) {
+            return res.status(501).send({ status: false, message: "Conpany not update!" });
+        }
+        res.status(200).send({ status: true, message: "Company Update Success" });
+
+    } catch (error) {
+        return res.status(400).send({ status: false, message: "Social Network faild!" });
+    }
+};
