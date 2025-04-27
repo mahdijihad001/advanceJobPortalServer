a47d0c65e0eb12533799631dae2ed7265aab2f82
mahdiJihad <mahdijihad001@gmail.com>
2025-04-27 10:04:56 +0600

    build company apis

---
+++ src/Company/companyRouter.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { updateCompanyController, companySocialNetworkUpdate, updateCompanyAddress, getfullSingleCompany } = require("./companyController");
+const companyRouter = express.Router();
+// Company
+companyRouter.patch("/update/:id" , updateCompanyController);
+companyRouter.get("/single/:id" , getfullSingleCompany);
+// Social Network
+companyRouter.param("/social/:id" , companySocialNetworkUpdate);
+// Company address 
+companyRouter.patch("/addrss/:id" , updateCompanyAddress);
+
+
+
+
+module.exports = companyRouter
